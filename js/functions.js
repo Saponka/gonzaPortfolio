1ccef89daef00df2d5ab92c791df31837f60aee7
Franco <francoherrera_8@hotmail.com.ar>
2024-05-29 18:03:17 -0300

    class brand + assets/text and js

---
+++ js/functions.js
@@ -0,0 +1,6 @@
+
+
+    document.addEventListener("DOMContentLoaded", function() {
+        var h4Content = document.getElementById('title-proyects').innerText;
+        document.getElementById('navbar-marca').innerText = h4Content;
+    });
